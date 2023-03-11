60a93c59a03fa8e00d936a47486e71e70b34b1d5
Ahmet Can Buyukyilmaz <abuyukyi@purdue.edu>
2023-03-11 17:28:17 +0300

    Added localization to popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -20,23 +20,17 @@ function createPopup() {
             'fill': 'var(--clr-primary)'
         }));
 
-    const title = createHTMLElement('p', {'class': 'semi-bold'});
-    title.innerText = 'Pick your test';
+    const title = createHTMLElement('p', {'class': 'semi-bold', 'data-locale': 'test-select-title'});
 
-    const completeButton = createHTMLElement('button', {'class': 'button primary'});
-    completeButton.innerText = 'Complete Test';
-    const completeInfo = createHTMLElement('p', {'class': 'question-count'});
-    completeInfo.innerText = '(120 questions)';
+    const completeButton = createHTMLElement('button', {'class': 'button primary', 'data-locale': 'test-select-complete-button'});
+    const completeInfo = createHTMLElement('p', {'class': 'question-count', 'data-locale': 'test-select-complete-info'});
 
     completeButton.addEventListener('click', () => {window.location.href = 'questions.html';});
 
-    const divider = document.createElement('p');
-    divider.innerText = 'or';
+    const divider = createHTMLElement('p', {'data-locale': 'test-select-divider'});
 
-    const basicButton = createHTMLElement('button', {'class': 'button secondary'});
-    basicButton.innerText = 'Basic Test'
-    const basicInfo = createHTMLElement('p', {'class': 'question-count'});
-    basicInfo.innerText = '(60 questions)';
+    const basicButton = createHTMLElement('button', {'class': 'button secondary', 'data-locale': 'test-select-basic-button'});
+    const basicInfo = createHTMLElement('p', {'class': 'question-count', 'data-locale': 'test-select-basic-info'});
 
     basicButton.addEventListener('click', () => {window.location.href = 'questions.html';});
 
