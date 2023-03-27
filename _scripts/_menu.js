d9c612534bdc583eee94eb81aaae70e383b2672d
Ahmet Can Buyukyilmaz <abuyukyi@purdue.edu>
2023-03-27 22:31:47 +0300

    fixed menu height bug

--- _scripts/_menu.js
+++ _scripts/_menu.js
@@ -1,28 +1,30 @@
-const logo = document.querySelector('#logo-container');
-logo.addEventListener('click', () => {
-    window.location.href = 'index.html';
-});
+(function () {
+    const logo = document.querySelector('#logo-container');
+    logo.addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
 
-const menuIcon = document.querySelector('#menu-icon');
-const menu = document.querySelector('#menu');
-const menuList = document.querySelector('#menu-list')
+    const menuIcon = document.querySelector('#menu-icon');
+    const menu = document.querySelector('#menu');
+    const menuList = document.querySelector('#menu-list')
 
-menuIcon.addEventListener('click', () => {
-    const isOpen = menuIcon.getAttribute('class') === 'open';
-    menuIcon.setAttribute('class', isOpen ? 'closed' : 'open');
-    menu.style.setProperty('height', isOpen ? '0px' : `${menuList.clientHeight}px`);
-});
+    menuIcon.addEventListener('click', () => {
+        const isOpen = menuIcon.getAttribute('class') === 'open';
+        menuIcon.setAttribute('class', isOpen ? 'closed' : 'open');
+        menu.style.setProperty('height', isOpen ? '0px' : `${menuList.clientHeight}px`);
+    });
 
-const mediaQuery = window.matchMedia('(min-width: 50em)');
-setMedia(menu, mediaQuery);
-
-mediaQuery.addEventListener('change', (mediaQuery) => {
+    const mediaQuery = window.matchMedia('(min-width: 50em)');
     setMedia(menu, mediaQuery);
 
-    if (menuIcon.getAttribute('class') === 'open') {
-        menuIcon.setAttribute('class', 'closed');
-    }
-});
+    mediaQuery.addEventListener('change', (mediaQuery) => {
+        setMedia(menu, mediaQuery);
+
+        if (menuIcon.getAttribute('class') === 'open') {
+            menuIcon.setAttribute('class', 'closed');
+        }
+    });
+})();
 
 function setMedia(menu, mediaQuery) {
     const isDesktop = mediaQuery.matches;
