dbeb6b3c7ee8b39b3a8d8fe51d07021ba73b495b
Ahmet Can Buyukyilmaz <abuyukyi@purdue.edu>
2023-03-29 21:06:00 +0300

    minor margin and <g> fixes

--- _scripts/_language.js
+++ _scripts/_language.js
@@ -8,13 +8,11 @@
     selected.setAttribute('id', `${lang}-selected`);
     selected.setAttribute('class', 'language selected');
 
-    selected.innerHTML = `<img src="/assets/navigation/flags/${lang}.png" class="flag" alt=""><svg class="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z"/></g></g></svg>`;
+    selected.innerHTML = `<img src="/assets/navigation/flags/${lang}.png" class="flag" alt=""><svg class="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z"/></svg>`;
 
     selector.appendChild(selected);
 
-    selected.addEventListener('click', () => {
-        handleExpand(selector)
-    });
+    selected.addEventListener('click', () => {handleExpand(selector)});
 
     const dropdown = document.createElement('div');
     dropdown.setAttribute('id', 'language-dropdown');
@@ -25,16 +23,14 @@
         option.setAttribute('id', `${locale}-selected`);
         option.setAttribute('class', `language ${locale === lang ? 'hidden' : ''}`);
 
-        option.innerHTML = `<img src="/assets/navigation/flags/${locale}.png" class="flag option" alt=""><svg class="arrow invisible" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z"/></g></g></svg>`;
+        option.innerHTML = `<img src="/assets/navigation/flags/${locale}.png" class="flag option" alt=""><svg class="arrow invisible" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z"/></svg>`;
 
         dropdown.appendChild(option);
     }
 
     selector.appendChild(dropdown);
 
-    window.addEventListener('click', (event) => {
-        handleClickOut(event, selector)
-    });
+    window.addEventListener('click', (event) => {handleClickOut(event, selector)});
 })();
 
 function handleExpand(selector) {
